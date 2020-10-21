41475c4a2bc994ad6f9e5a18ee73432f38519d1a
PaulSun256 <paulsun700@gmail.com>
2020-10-21 17:44:41 -0400

    commented

---
+++ Sun_Paul_Blackjack/src/main/Dealer.java
@@ -0,0 +1,31 @@
+/*
++========================================+
+|              Dealer class              |
++========================================+
+|          Paul Sun, 2020/10/20          |
++========================================+
+|              ICS3U Ms. S               |
++========================================+
+|   This is the class for the dealer in  |
+|   blackjack. He / she can deal cards,  |
+|  and you cannot see all of their cards |
+|       using the showHand method.       |
++========================================+
+*/
+
+public class Dealer	{
+    
+    public Dealer() {
+
+        super("Dealer", 999999999);
+    }
+
+// methods
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+    public String showHand()    {
+        
+        // we aren't supposed to see the dealer's hand
+        if(totalCards == 0) return "";
+        return getSuits(hand[i]) + "?";
+    }
+}
