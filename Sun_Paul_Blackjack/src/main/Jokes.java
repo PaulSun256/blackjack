8817b79ec1824f138a256c67ea5a99fd19d79ff5
PaulSun256 <paulsun700@gmail.com>
2020-10-21 18:52:52 -0400

    started jokes.java

---
+++ Sun_Paul_Blackjack/src/main/Jokes.java
@@ -0,0 +1,20 @@
+/*
++========================================+
+|              Jokes class               |
++========================================+
+|          Paul Sun, 2020/10/20          |
++========================================+
+|              ICS3U Ms. S               |
++========================================+
+| This is the class for jokes. Jokes are |
+|   static, meaning that the jokes 'in   |
+| stock' are unchanging from instance to |
+|   instance. The jokes are read from a  |
+|                  file.                 |
++========================================+
+*/
+import java.util.*;
+
+public class Jokes	{
+    
+}
