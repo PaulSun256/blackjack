5d42bfe89c3a6e84bf701d703619deb5d96926ba
PaulSun256 <paulsun700@gmail.com>
2020-10-21 16:02:01 -0400

    hopefully completed deck class and commented

--- Sun_Paul_Blackjack/src/main/Deck.java
+++ Sun_Paul_Blackjack/src/main/Deck.java
@@ -12,8 +12,59 @@
 |                  card.                 |
 +========================================+
 */
+import java.util.*;
 
 public class Deck	{
-    int deck[] = new int[52];
-    
+
+    // creating the deck array and an integer that indicates which card to use next
+    private int deck[] = new int[52];
+    private int currentCard = 0;
+
+    public Deck()   {
+        
+        newDeck();
+    }
+
+    public void newDeck()   {
+
+        // fills the array with a sorted pile of cards, AKA a sorted array of consecutive integers
+        for (int i = 0; i < 52; i++)    deck[i] = i;
+    }
+
+    public void shuffle()   {
+        
+        // we need a temporary variable to swap with
+        int temp;
+        int randomIndex;
+
+        Random rand = new Random();
+        
+        for (int i = 0; i < 52; i++)    {
+            
+            // we pick a random index to swap our current index with, and we repeat this, and we get a shuffled deck
+            randomIndex = rand.nextInt(52);
+            temp = deck[randomIndex];
+            deck[randomIndex] = deck[i];
+            deck[i] = temp;
+        }
+    }
+
+    public int nextCard()   {
+        
+        // we make a new deck if needed
+        if (currentCard >= 41)   {
+
+            System.out.println("We are running low on cards. Creating and shuffling new deck...");
+
+            newDeck();
+            shuffle();
+            currentCard = 0;
+        }
+        // return the next card and increment. a ++ after the variable will increment AFTER running
+        // eg:
+        // i = 1
+        // j = i++
+        // i is now 2, but j is 1
+        return deck[currentCard++];
+    }
 }
